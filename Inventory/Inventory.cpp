d43210bec319542edcf2da543cf4ff45aa039ebc
George <moustak85@hotmail.com>
2020-06-06 19:03:30 +0300

    Added Inventory Documentation

--- Inventory/Inventory.cpp
+++ Inventory/Inventory.cpp
@@ -5,120 +5,135 @@ Inventory::Inventory()
     ;
 }
 
+Inventory::Inventory(const Inventory &copy) : invItems(copy.invItems)
+{
+    ;
+}
 
-Inventory::Inventory(const Inventory &copy)
-{
-    //this->inventoryItems = copy.inventoryItems;
-}
-Inventory::Inventory(Item *items, int itemCount)
-{
-    int i;
-    for (i = 0; i < itemCount; i++)
-    {
-        //this->inventoryItems.push_back(&items[i]);
-    }
-}
 Item &Inventory::getItemAt(int index)
 {
     return invItems[index];
 }
-Inventory& Inventory::operator=(Inventory &inv)
+Inventory &Inventory::operator=(Inventory &inv)
 {
-    this->invItems = inv.invItems;
+    invItems = inv.invItems;
     return *this;
 }
 
 void Inventory::addItem(Item &item)
 {
-    
-    this->invItems.push_back(item);
+    invItems.push_back(item);
 }
 void Inventory::removeAll()
 {
-    
-    invItems.clear();
 
+    invItems.clear();
 }
 void Inventory::removeItemAt(int index)
 {
     int i = 0;
-    for(DynamicArray<Item>::iterator it= invItems.begin(); it!= invItems.end();){
-        if(i == index){
+    for (DynamicArray<Item>::iterator it = invItems.begin(); it != invItems.end();)
+    {
+        if (i == index)
+        {
             it = invItems.erase(it);
         }
-        else{
+        else
+        {
             it++;
         }
         i++;
     }
-    //invItems.erase(index);
 }
-DynamicArray<int> Inventory::itemTypeCount(){
+DynamicArray<int> Inventory::itemTypeCount()
+{
     DynamicArray<int> dArray;
-    dArray.push_back(0);//lighter
-    dArray.push_back(0);//woodStick
-    dArray.push_back(0);//Leafs
-    dArray.push_back(0);//rock
-    dArray.push_back(0);//coconut
-    dArray.push_back(0);//axe
-    dArray.push_back(0);//openCoconut
-    dArray.push_back(0);//lightedTorch
-    for(auto x :invItems){
-        if(x.getType() == lighter){
+    dArray.push_back(0); //Θα χρησιμοποιηθεί για το πλήθος αντικειμένων lighter. Το ίδιο ισχύει και γι ατους άλλους τύπους
+    dArray.push_back(0); //woodStick
+    dArray.push_back(0); //Leafs
+    dArray.push_back(0); //rock
+    dArray.push_back(0); //coconut
+    dArray.push_back(0); //axe
+    dArray.push_back(0); //openCoconut
+    dArray.push_back(0); //lightedTorch
+    for (auto x : invItems)
+    {
+        //Για καθε αντικείμενο στη λίστα των αντικειμένων εάν ο τύπος του είναι πχ lighter τότε αυξάνουμε κατα 1 το περιεχόμενο της θέσης 0 του δυναμικού πίνακα dArray 
+        //Εάν είναι woodstick τότε το περιεχόμενο της 1 κ.ο.κ
+        if (x.getType() == lighter)
+        {
             dArray[0]++;
         }
-        else if(x.getType() == woodstick){
+        else if (x.getType() == woodstick)
+        {
             dArray[1]++;
         }
-        else if(x.getType() == leafs){
+        else if (x.getType() == leafs)
+        {
             dArray[2]++;
         }
-        else if(x.getType() == rock){
+        else if (x.getType() == rock)
+        {
             dArray[3]++;
         }
-        else if(x.getType() == coconut){
+        else if (x.getType() == coconut)
+        {
             dArray[4]++;
         }
-        else if(x.getType() == axe){
+        else if (x.getType() == axe)
+        {
             dArray[5]++;
         }
-        else if(x.getType() == opencoconut){
+        else if (x.getType() == opencoconut)
+        {
             dArray[6]++;
         }
-        else if(x.getType() == lightedtorch){
+        else if (x.getType() == lightedtorch)
+        {
             dArray[7]++;
         }
     }
     return dArray;
 }
-DynamicArray<Item> Inventory::getInventoryItems(){
+DynamicArray<Item> Inventory::getInventoryItems()
+{
+    //Δημιουργείται και επιστρέφεται αντίγραφο των αντικειμένων που περιέχει η "τσαντα" του χρήστη
     DynamicArray<Item> cpArray;
     cpArray = invItems;
     return cpArray;
 }
-Inventory::~Inventory(){
+Inventory::~Inventory()
+{
     ;
 }
 DynamicArray<Item> Inventory::removeAfterCrafting(itemType itemCraftedType)
 {
-    DynamicArray<Item> itemsRemoved;
-    
+    DynamicArray<Item> itemsRemoved;//ΔΥναμικος πινακας που περιεχει τα αντικειμενα τα οποια ο χρηστης χρησιμοποιησε για να φτιαξει το παράγωγο αντικείμενο
+
     int count = 0;
+    /*Flags που χρησιμοποιούνται για να ξέρω εάν εχει ήδη διαγραφεί κάποιο αντικείμενο ενος συγκεκριμένου τύπου */
     bool rockFlag = false;
     bool cocFlag = false;
     bool leafFlag = false;
     bool woodFlag = false;
-    
+
     int size = invItems.size();
     int count2 = 0;
-    for (DynamicArray<Item>::iterator i = invItems.begin(); i !=invItems.end() ;){
+    for (DynamicArray<Item>::iterator i = invItems.begin(); i != invItems.end();)
+    {
         if (itemCraftedType == axe && (invItems[count2].getType() == rock && rockFlag == false))
         {
+            /*Εάν ο χρήστης δημιούργησε τσεκούρι ΚΑΙ ο τυπος του αντικειμένου είναι Πετρα(rock) ΚΑΙ ΔΕΝ έχει διαγράψει αντικείμενο πέτρας πρίν
+              Τότε προσθετει το αντικείμενο που βρισκεται στη θεση count2 στον πινακα διεγραμμένων αντικειμένων, διαγράφει το τορινό αντικείμενο στο οποιο δείχνει ο iterator i
+              Στη συνέχεια βάζει το Flag rockFlag να είναι ΑΛΗΘΗΣ ωστε να μην ξαναδιαγράψει στις επόμενες επαναλήψεις αντικείμενο τύπου Πέτρα
+              Τέλος αυξάνει το count κατα 1.
+              Το count χρησιμοποιείται για να σταματάει η επανάληψη μόλις ο χρήστης έχει διαγράψει 2 αντικείμενα οποιουδήποτε τύπου.Αυτο γιατι ΟΛΑ τα παράγωγα παράγονται αυστηρώς απο 2 μόνο πρωτογενή αντικείμενα.
+              Το ίδιο ακριβώς ισχύει για τους υπόλοιπους τύπους.
+            */
             itemsRemoved.push_back(invItems[count2]);
             invItems.erase(i);
             rockFlag = true;
             count++;
-            
         }
         else if (itemCraftedType == axe && (invItems[count2].getType() == woodstick && woodFlag == false))
         {
@@ -126,7 +141,6 @@ DynamicArray<Item> Inventory::removeAfterCrafting(itemType itemCraftedType)
             invItems.erase(i);
             woodFlag = true;
             count++;
-           
         }
         else if (itemCraftedType == opencoconut && (invItems[count2].getType() == coconut && cocFlag == false))
         {
@@ -134,7 +148,6 @@ DynamicArray<Item> Inventory::removeAfterCrafting(itemType itemCraftedType)
             invItems.erase(i);
             cocFlag = true;
             count++;
-           
         }
         else if (itemCraftedType == opencoconut && (invItems[count2].getType() == rock && rockFlag == false))
         {
@@ -142,7 +155,6 @@ DynamicArray<Item> Inventory::removeAfterCrafting(itemType itemCraftedType)
             invItems.erase(i);
             rockFlag = true;
             count++;
-            
         }
         else if (itemCraftedType == lightedtorch && (invItems[count2].getType() == leafs && leafFlag == false))
         {
@@ -150,7 +162,6 @@ DynamicArray<Item> Inventory::removeAfterCrafting(itemType itemCraftedType)
             invItems.erase(i);
             leafFlag = true;
             count++;
-            
         }
         else if (itemCraftedType == lightedtorch && (invItems[count2].getType() == woodstick && woodFlag == false))
         {
@@ -158,15 +169,15 @@ DynamicArray<Item> Inventory::removeAfterCrafting(itemType itemCraftedType)
             invItems.erase(i);
             woodFlag = true;
             count++;
-           
         }
-        else{
+        else
+        {
             i++;
             count2++;
         }
         if (count == 2)
             break;
     }
-    
+
     return itemsRemoved;
 }
--- Inventory/Inventory.h
+++ Inventory/Inventory.h
@@ -5,31 +5,72 @@ using namespace std;
 #include <vector>
 #include "../Items/Item/Item.h"
 #include "../DynamicArray/DynamicArray.h"
+/**
+ * @brief Κλάση που περιέχει τα αντικείμενα του χρήστη.
+ * 
+ */
 class Inventory{
     private:
-        DynamicArray<Item> invItems;
+        DynamicArray<Item> invItems; 
     public:
         Inventory();
-        Inventory(Item *items,int itemCount);
+
         Inventory(const Inventory & copy);
         ~Inventory();
 
+        /**
+         * @brief Προσθέτει ενα αντικείμενο στο τέλος του δυναμικού πίνακα που περιέχει τα αντικείμενα.
+         * 
+         * @param item 
+         */
         void addItem(Item &item);
         
+        /**
+         * @brief Getter για τον δυναμικό πίνακα αντικειμένων
+         * 
+         * @return DynamicArray<Item> 
+         */
         DynamicArray<Item> getInventoryItems();
-
+        /**
+         * @brief Getter για ενα συγκεκριμένο αντικείμενο
+         * 
+         * @param index το index του αντικειμένου
+         * @return Item& αναφορά στο αντικείμενο που βρίσκεται στη συκγεκριμένη θέση
+         */
         Item & getItemAt(int index);
 
+        /**
+         * @brief Διαγράφει αντικείμενο στη συγκεκριμένη θέση που προσδιορίζεται απο την παράμετρο index
+         * 
+         * @param index 
+         */
         void removeItemAt(int index);
         
+        /**
+         * @brief Επιστρέφει δυναμικο πινακα που περιέχει το πλήθος των αντικειμένων του κάθε τύπου <br>
+         * Για παράδειγμα εάν έχουμε 5 αντικείμενα τύπου woodstick τότε στην αντίστοιχη θέση του τύπου woodstick θα περιέχει 5.
+         * 
+         * @return DynamicArray<int> 
+         */
         DynamicArray<int> itemTypeCount();
 
         size_t getSize(){return invItems.size();}
 
         Inventory& operator=( Inventory & inv);
-
+        
+        /**
+         * @brief Διαγράφει όλα τα αντικείμενα που βρίσκονται στον δυναμικό πίνακα
+         * 
+         */
         void removeAll();
 
+        /**
+         * @brief Επιστρέφει δυναμικό πίνακα που περιέχει τα πρωτογενή αντικείμενα που διαγράφτηκαν για να κάνει craft ο χρήστης.<br>
+         * Εαν ο χρηστης για παραδειγμα δημιουργήσει ενα Axe απο Rock και woodstick τότε θα προσθέσει τα 2 αντικείμενα αυτα στον δυναμικό πίνακα και μετά θα τα διαγράψει.Τέλος θα επιστεψει τον πίνακα.
+         * 
+         * @param itemCraftedType Τύπος του παράγωγου αντικειμένου που ο χρήστης έκανε craft
+         * @return DynamicArray<Item> Δυναμικος Πίνακας που περιέχει τα πρωτογενή αντικείμενα που διαγράφτηκαν.
+         */
         DynamicArray<Item> removeAfterCrafting(itemType itemCraftedType);
 };
 
