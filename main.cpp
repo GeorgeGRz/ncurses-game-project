988af3024cce75e1a2cd36a6cc8671c15a3c3adf
George <moustak85@hotmail.com>
2020-06-19 13:46:25 +0300

    docs

--- main.cpp
+++ main.cpp
@@ -23,14 +23,14 @@ using namespace std;
  * </ul>
  * \section install_sec Οδηγίες εγκατάστασης
  *  \subsection step1 (A) Repository
- *  Μετακινιθείτε στον κατάλογο οπου επιθυμείτε να κάνετε clone και πληκτρολογείστε στο τερματικο @code git clone https://github.com/GeorgeGRz/ncurses-game-project.git @endcode
+ *  Μετακινηθείτε στον κατάλογο οπου επιθυμείτε να κάνετε clone και πληκτρολογηστε στο τερματικο @code git clone https://github.com/GeorgeGRz/ncurses-game-project.git @endcode
  *  \subsection step2 (B) Συμπιεσμένο αρχείο
- *  Αποσυμπιέστε το αρχείο στον επιθυμιτό κατάλογο με το command @code tar -xzvf ncurses-game-project.tar.gz @endcode
+ *  Αποσυμπιέστε το αρχείο στον επιθυμητό κατάλογο με το command @code tar -xzvf ncurses-game-project.tar.gz @endcode
  * \subsection step3 Μετακίνιση στον κατάλογο που εγινε clone
- *  Στη συνέχεια πληκτρολογείστε @code cd ncurses-game-project/ && make all @endcode
+ *  Στη συνέχεια πληκτρολογηστε @code cd ncurses-game-project/ && make all @endcode
  *  
  *  \subsection step4 Τρέξτε το παιχνίδι.
- *  Για να τρέξετε το παιχνίδι απλά πληκτρολογείστε ενώ βρίοκεστε στον ίδιο κατάλογο @code ./adventureGame @endcode
+ *  Για να τρέξετε το παιχνίδι απλά πληκτρολογηστε ενώ βρίσκεστε στον ίδιο κατάλογο @code ./adventureGame @endcode
  *  
  */
 ostream & operator<<(ostream& s, Player p){
